2cacff2ed65b1d076e126a3164c3d0ae6edd69d2
Adam <adamg@sendible.com>
2019-07-18 17:06:49 +0100

    Add payment_ method resource

--- lib/stripe/util.rb
+++ lib/stripe/util.rb
@@ -48,6 +48,7 @@ module Stripe
         LoginLink::OBJECT_NAME            => LoginLink,
         Order::OBJECT_NAME                => Order,
         OrderReturn::OBJECT_NAME          => OrderReturn,
+        PaymentMethod::OBJECT_NAME        => PaymentMethod,
         Payout::OBJECT_NAME               => Payout,
         Plan::OBJECT_NAME                 => Plan,
         Product::OBJECT_NAME              => Product,
